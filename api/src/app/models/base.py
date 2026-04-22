13b7212a1aa07597acd3beac09c91aa767e7ecd2
Ali Khakpouri <ali.khakpouri@gmail.com>
2026-04-22 15:48:14 -0400

    added the pycopg2

--- api/src/app/models/base.py
+++ api/src/app/models/base.py
@@ -1,9 +1,9 @@
-from dataclasses import dataclass
 from datetime import datetime
 from abc import ABC
+from sqlalchemy.orm import DeclarativeBase,Mapped, mapped_column
 
-@dataclass
-class Base(ABC):
-    id: int
-    created_date: datetime
-    updated_date: datetime
+
+class Base(DeclarativeBase):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    created_date: Mapped[datetime] = mapped_column(default=datetime.now)
+    updated_date: Mapped[datetime] = mapped_column(default=datetime.now)
--- api/src/app/models/user.py
+++ api/src/app/models/user.py
@@ -1,11 +1,12 @@
-from dataclasses import dataclass
-
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
 from src.app.models.base import Base
 
-@dataclass
+
 class User(Base):
-    name: str
-    email: str
-    first_name: str
-    last_name: str
-    is_active: bool
+    __tablename__ = "users"
+    name: Mapped[str] = mapped_column(String(30), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), nullable=False)
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(75), nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=False, nullable=False)
